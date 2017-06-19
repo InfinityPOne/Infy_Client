bee36ecb5cf6f51cb727c08a32738fc995e49607
GreenCillah <thecoolgreenman@gmail.com>
2017-06-19 21:29:53 +1000

    BaseLine For Infy

---
+++ src/pages/clowns.vue
@@ -0,0 +1,15 @@
+<template>
+  <f7-page>
+    <f7-navbar title="Clowns" back-link="Back" sliding></f7-navbar>
+    <f7-block inner>
+      <p>Here ya go clowns</p>
+      <h1>CDDSNPPBAD</h1>
+              
+    </f7-block>
+  </f7-page>
+</template>
+
+<script>
+    export default {}
+
+</script>
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,19 @@
 export default [
-  {
-      path: '/about/',
-      component: require('./pages/about.vue')
+    {
+        path: '/about/',
+        component: require('./pages/about.vue')
   },
-  {
-      path: '/form/',
-      component: require('./pages/form.vue')
+    {
+        path: '/form/',
+        component: require('./pages/form.vue')
   },
-  {
-      path: '/dynamic-route/blog/:blogId/post/:postId/',
-      component: require('./pages/dynamic-route.vue')
+    {
+        path: '/dynamic-route/blog/:blogId/post/:postId/',
+        component: require('./pages/dynamic-route.vue')
+  },
+    {
+        path: '/clowns/',
+        component: require('./pages/clowns.vue')
   }
+
 ]
